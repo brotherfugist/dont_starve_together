3c0e2c10c7f1beabc5651acb71d7d16647440c33
Weiyi Lou <aloua@google.com>
2020-05-08 21:58:54 -0700

    After playing two days with wen.

---
+++ 1088446501/Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,18 @@
+return {
+  ["workshop-2078243581"]={
+    configuration_options={ Blue=0, Display="target", Green=0, Projectile=true, Red=1, Type="hit" },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
